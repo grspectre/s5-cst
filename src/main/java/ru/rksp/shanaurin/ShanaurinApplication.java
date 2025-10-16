f7b1e84f0efd03889056231ba518cb01e302d301
Anton A Shanaurin <stud0000298878@utmn.ru>
2025-10-16 11:52:40 +0300

    create springboot project

---
+++ src/main/java/ru/rksp/shanaurin/ShanaurinApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.shanaurin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShanaurinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShanaurinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/shanaurin/ShanaurinApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.shanaurin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShanaurinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
